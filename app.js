1154094adf49893fdfaf35c22d1a9be402629b2d
ptrikkur <poornima@Poornimas-MacBook-Air.local>
2017-06-19 17:01:27 +0530

    Commit10

--- app.js
+++ app.js
@@ -56,6 +56,18 @@ var discovery = new DiscoveryV1({
   version_date: DiscoveryV1.VERSION_DATE_2017_04_27
 });
 
+discovery.query({
+    environment_id: 'cdd6c5fa-f76f-47ea-ad49-6c669e9a652f',
+    collection_id: '2e354788-b6ce-48bb-82c4-86b564b890b5',
+    query: 'enriched_text.entities.text%3AVodafone&count=&offset=&aggregation=&filter=&passages=true&highlight=true&return=passages.passage_text'
+  }, function(err, response) {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(JSON.stringify(response, null, 2));
+        }
+   });
+
 var usersMap;
 var userDetails;
 
